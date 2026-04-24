2ef11f041c1cce231507040ed6a3c0ba6877a303
chowanza <luiscandialesfajardo@gmail.com>
2026-04-24 02:19:57 -0400

    Finished almost everything without form

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,14 +12,14 @@ export const Footer = ({ dict, lang }: FooterProps) => {
   return (
     <footer className="bg-brand-primary text-slate-300 py-20">
       <div className="container mx-auto px-4 lg:px-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 lg:gap-8">
-        
+
         {/* Logo Col */}
         <div className="flex items-start">
           <div className="relative w-32 h-32 md:w-36 md:h-36">
             <Image src="/logo_in_white.svg" alt="ARKON Logo" fill className="object-contain object-left" />
           </div>
         </div>
-        
+
         {/* Links Col */}
         <div>
           <h4 className="text-white font-bold mb-6 text-[15px] tracking-wide">{dict.footer.quickLinks.title}</h4>
@@ -29,7 +29,7 @@ export const Footer = ({ dict, lang }: FooterProps) => {
             <li><Link href={`/${lang}/servicios`} className="hover:text-white transition-smooth">{dict.footer.quickLinks.servicios}</Link></li>
           </ul>
         </div>
-        
+
         {/* Contacto Col */}
         <div>
           <h4 className="text-white font-bold mb-6 text-[15px] tracking-wide">{dict.footer.contact.title}</h4>
@@ -51,7 +51,7 @@ export const Footer = ({ dict, lang }: FooterProps) => {
             </li>
           </ul>
         </div>
-        
+
         {/* Direcciones Col */}
         <div>
           <div className="mb-8">
@@ -75,14 +75,9 @@ export const Footer = ({ dict, lang }: FooterProps) => {
       {/* Copyright Bottom Bar */}
       <div className="border-t border-white/10 mt-16">
         <div className="container mx-auto px-4 lg:px-12 py-6 flex flex-col md:flex-row items-center justify-between text-center md:text-left text-[13px] font-light text-slate-400">
-          <p>
-            &copy; {new Date().getFullYear()} ARKON, C.A. {dict.footer.rights}
+          <p className="w-full text-center md:text-left">
+            &copy; {new Date().getFullYear()} AMPER TECH {dict.footer.rights}
           </p>
-          <div className="mt-4 md:mt-0 flex items-center gap-4">
-            <Link href={`/${lang}`} className="hover:text-white transition-colors">Politica de Privacidad</Link>
-            <span className="w-1 h-1 rounded-full bg-slate-600"></span>
-            <Link href={`/${lang}`} className="hover:text-white transition-colors">Terminos de Servicio</Link>
-          </div>
         </div>
       </div>
     </footer>
